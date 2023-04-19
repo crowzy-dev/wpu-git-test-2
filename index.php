a6e60f81484cfcddd01e1a145731784f6832b1fd
crowzy-dev <131239277+crowzy-dev@users.noreply.github.com>
2023-04-20 06:20:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
     <h1>hello world</h1>
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat reprehenderit quia, praesentium, facere blanditiis corporis rem incidunt labore, optio id molestiae beatae possimus exercitationem rerum aspernatur nulla atque enim earum?</p>
+    <p> lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat reprehenderit quia, praesentium, facere blanditiis corporis rem incidunt labore, optio id molestiae beatae possimus exercitationem rerum aspernatur nulla atque enim earum?</p>
 
     <ul>
         <li>ddd</li>
